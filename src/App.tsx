1a297227eee17e9fceee0f3d03ceb8077f290f86
[React1] 김선규 <seongyu1066@naver.com>
2024-10-08 22:27:38 +0900

    feat: 프로젝트 상세보기 UI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateProject from './pages/CreateProject/CreateProject';
+import ViewProject from './pages/ViewProject/ViewProject';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CreateProject />} />
+        <Route path="/create" element={<CreateProject />} />
+        <Route path="/view" element={<ViewProject />} />
       </Routes>
     </Router>
   );
