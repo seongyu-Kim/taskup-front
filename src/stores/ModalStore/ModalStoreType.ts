d53a1f158ddafd577e39f88452ab20870b0a0433
[React1] 박주호 <parkjh001217@gmail.com>
2024-10-10 17:01:57 +0900

    feat: 모달 종류, 열고 닫기 위한 상태 추가

--- src/stores/ModalStore/ModalStoreType.ts
+++ src/stores/ModalStore/ModalStoreType.ts
@@ -2,3 +2,8 @@ export interface ModalStoreType {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
 }
+
+export interface ModalStateType {
+  modalState: '' | 'Profile' | 'Notice';
+  setModalState: (modalState: '' | 'Profile' | 'Notice') => void;
+}
