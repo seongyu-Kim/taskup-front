8be4908ac8ea81ace23ffacfa78d62e91cc5ed8c
[React1] 박주호 <parkjh001217@gmail.com>
2024-10-10 21:02:41 +0900

    작업 내용 저장

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateProject from './pages/CreateProject/CreateProject';
+import ViewProject from './pages/ViewProject/ViewProject';
 
 function App() {
   return (
