0ca5ea71e69bb3f561f1d87dbbb432bf93ebb3f5
[React1] 박주호 <parkjh001217@gmail.com>
2024-10-10 17:01:45 +0900

    feat: 모달 종류, 열고 닫기 위한 상태 추가

--- src/stores/ModalStore/ModalStore.ts
+++ src/stores/ModalStore/ModalStore.ts
@@ -1,9 +1,17 @@
 import { create } from 'zustand';
-import { ModalStoreType } from './ModalStoreType';
+import { ModalStateType, ModalStoreType } from './ModalStoreType';
 
-const useModal = create<ModalStoreType>((set) => ({
+//모달 ON OFF
+export const useModal = create<ModalStoreType>((set) => ({
   isOpen: false,
   setIsOpen: (value) => {
     set({ isOpen: value });
+  },
+}));
+
+export const useModalState = create<ModalStateType>((set) => ({
+  modalState: '',
+  setModalState: (value) => {
+    set({ modalState: value });
   },
 }));
