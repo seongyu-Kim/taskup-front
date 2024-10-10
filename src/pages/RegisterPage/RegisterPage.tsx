3886d1b49e9c24ec3e9300bcd86f8c38e42dd262
bella <103259907+WolgyeBella@users.noreply.github.com>
2024-10-10 13:12:25 +0900

    fix: 회원가입 페이지 마크업 수정

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -21,9 +21,15 @@ export default function LoginPage() {
           </h1>
           <Form action="">
             <InputBox>
-              <label htmlFor="id">아이디</label>
-              <input type="text" name="id" id="id" placeholder="아이디를 입력하세요." />
-              <p>이미 존재하는 아이디입니다.</p>
+              <label htmlFor="email">이메일</label>
+              <input type="email" name="email" id="email" placeholder="이메일을 입력하세요." />
+              <p>이미 존재하는 이메일 입니다.</p>
+            </InputBox>
+
+            <InputBox>
+              <label htmlFor="name">이메일</label>
+              <input type="text" name="name" id="name" placeholder="이메일을 입력하세요." />
+              <p>이미 존재하는 이메일 입니다.</p>
             </InputBox>
 
             <InputBox>
@@ -35,24 +41,18 @@ export default function LoginPage() {
                 maxLength={10}
                 placeholder="비밀번호를 입력하세요."
               />
-              <p></p>
             </InputBox>
 
             <InputBox>
-              <label htmlFor="password">비밀번호</label>
+              <label htmlFor="password_check">비밀번호</label>
               <input
                 type="password"
-                name="password"
-                id="password"
+                name="password_check"
+                id="password_check"
                 maxLength={10}
                 placeholder="비밀번호를 다시 입력하세요."
               />
               <p>비밀번호가 일치하지 않습니다.</p>
-            </InputBox>
-
-            <InputBox>
-              <label htmlFor="email">이메일</label>
-              <input type="email" name="email" id="email" placeholder="이메일을 입력하세요." />
             </InputBox>
 
             <InputBox>
