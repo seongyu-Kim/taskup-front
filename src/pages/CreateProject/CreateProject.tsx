ba86a5ddc72d2c5f633b4f504279a23ddfec2428
[React1] 김선규 <seongyu1066@naver.com>
2024-10-11 14:17:52 +0900

    feat: create-project 기능 추가

--- src/pages/CreateProject/CreateProject.tsx
+++ src/pages/CreateProject/CreateProject.tsx
@@ -5,6 +5,7 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { format } from 'date-fns';
 import { FaCalendarAlt } from 'react-icons/fa';
+import axios from 'axios';
 
 interface ProjectData {
   title: string;
@@ -32,7 +33,22 @@ const CreateProject: React.FC = () => {
     setEndDate(date);
   };
 
-  const handleSubmit = async () => {};
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('/api/projects', {
+        title,
+        subtitle,
+        author,
+        description,
+        startDate: startDate ? format(startDate, 'yyyy-MM-dd') : null,
+        endDate: endDate ? format(endDate, 'yyyy-MM-dd') : null,
+        participants,
+      });
+      console.log('성공:', response.data);
+    } catch (error) {
+      console.error('실패:', error);
+    }
+  };
 
   return (
     <Styled.Container>
@@ -43,19 +59,39 @@ const CreateProject: React.FC = () => {
         </Styled.BackBtn>
         <Styled.Content>
           <Styled.ContentBox>
-            <Styled.Input type="text" placeholder="제목을 입력하세요" />
+            <Styled.Input
+              type="text"
+              placeholder="제목을 입력하세요"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
 
             <Styled.MarginTop top={20} />
 
-            <Styled.Input type="text" placeholder="부제목을 입력하세요" />
+            <Styled.Input
+              type="text"
+              placeholder="부제목을 입력하세요"
+              value={subtitle}
+              onChange={(e) => setSubtitle(e.target.value)}
+            />
 
             <Styled.MarginTop top={20} />
 
-            <Styled.Input type="text" placeholder="작성자를 입력하세요" />
+            <Styled.Input
+              type="text"
+              placeholder="작성자를 입력하세요"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+            />
 
             <Styled.MarginTop top={20} />
 
-            <Styled.TextArea rows={12} placeholder="프로젝트 내용" />
+            <Styled.TextArea
+              rows={12}
+              placeholder="프로젝트 내용"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
 
             <Styled.MarginTop top={30} />
 
@@ -99,12 +135,17 @@ const CreateProject: React.FC = () => {
 
             <Styled.MarginTop top={30} />
 
-            <Styled.Input type="text" placeholder="참가자를 입력하세요" />
+            <Styled.Input
+              type="text"
+              placeholder="참가자를 입력하세요"
+              value={participants}
+              onChange={(e) => setParticipants(e.target.value)}
+            />
 
             <Styled.MarginTop top={30} />
 
             <Styled.BtnBox>
-              <Styled.GreenBtn1>등록하기</Styled.GreenBtn1>
+              <Styled.GreenBtn1 onClick={handleSubmit}>등록하기</Styled.GreenBtn1>
               <Styled.GreenBtn2>취소하기</Styled.GreenBtn2>
             </Styled.BtnBox>
           </Styled.ContentBox>
