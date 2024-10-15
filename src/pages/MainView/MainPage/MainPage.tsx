9c77698adc2b154b9c48bc4cf04cd7d964687298
[React1] 박주호 <parkjh001217@gmail.com>
2024-10-15 14:19:47 +0900

    refactor: 불필요한 useEffect 삭제

--- src/pages/MainView/MainPage/MainPage.tsx
+++ src/pages/MainView/MainPage/MainPage.tsx
@@ -1,14 +1,7 @@
 import TaskList from './TaskList/TaskList';
 import CalenderView from './CalenderView/CalenderView';
 import { MainBox } from './MainPage.styled';
-import { useEffect } from 'react';
-import { useProfileImgStore } from '../../../stores/ProfileImgStore/ProfileImgStore';
-import defaultImg from '../../../assets/임시 프로필사진.png';
 export default function MainPage() {
-  const { imageUrl, setImageUrl } = useProfileImgStore();
-  useEffect(() => {
-    setImageUrl(defaultImg);
-  }, [imageUrl, setImageUrl]);
   return (
     <MainBox>
       <TaskList />
