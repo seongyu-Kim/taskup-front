4b83dc5e158701f391e043d2e17296064d836d93
[React1] 박주호 <parkjh001217@gmail.com>
2024-10-17 18:07:26 +0900

    feat: 월, 년에 따라서 데이터 요청

--- src/pages/MainView/MainPage/CalenderView/CalenderView.tsx
+++ src/pages/MainView/MainPage/CalenderView/CalenderView.tsx
@@ -5,6 +5,7 @@ import { handleDayCellContent } from '@utils/CalenderUtils';
 import { useEffect, useState } from 'react';
 import apiMainPage from '@apis/apiMainPage';
 import { useUserStore } from '@stores/UserStore/userStore';
+import { DatesSetArg } from '@fullcalendar/common';
 
 interface CalenderType {
   id: number;
@@ -13,36 +14,57 @@ interface CalenderType {
   startDate: string;
   endDate: string;
   members: string[];
-  author?: string;
+  name?: string;
 }
 
 export default function CalenderView() {
   const [callEvent, setCallEvent] = useState<CalenderType[]>([]);
+  const [calendarTitle, setCalendarTitle] = useState('');
   const { user } = useUserStore();
+  //캘린더 상단 메뉴
+  const header = {
+    start: 'prevYear,dayGridDay,dayGridWeek,dayGridMonth,nextYear',
+    center: 'title',
+    end: 'today prev,next',
+  };
+  //버튼 별 커스텀
 
   useEffect(() => {
     const callCalenderEventData = async () => {
       try {
-        //임시
-        const response = await apiMainPage.get('tasks?page=1&pageSize=10&status');
+        //임시로 전체 데이터 사용
+        const response = await apiMainPage.get(
+          `/tasks/calender?startDate=${calendarTitle}-29&type=month`,
+        );
         if (response) {
-          setCallEvent(response.data.data.data);
+          setCallEvent(response.data.data);
         }
       } catch (error) {
         console.log('CALENDERVIEW DATA CALL ERROR', error);
       }
     };
     callCalenderEventData().catch(console.error);
-  }, []);
+  }, [calendarTitle]);
 
+  const handleDateSet = ({ view }: DatesSetArg) => {
+    const date = view.currentStart;
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 월 (01부터 시작)
+
+    setCalendarTitle(`${year}-${month}`);
+  };
+
+  //날짜 텍스트 보기
   if (!user) {
     return null;
   }
+  if (!callEvent) {
+    return null;
+  }
 
-  // const userEventData = callEvent.filter((item) => item.members.includes(user.name));
-  const userEventData = callEvent.filter(
-    (item) => item.author!.includes(user.name) || item.members.includes(user.name),
-  );
+  const userEventData = callEvent.filter((item) => {
+    return item.name && item.name.includes(user.name);
+  });
 
   return (
     <CalenderBox>
@@ -58,6 +80,8 @@ export default function CalenderView() {
         editable={true}
         locale="ko"
         dayCellContent={handleDayCellContent}
+        headerToolbar={header}
+        datesSet={handleDateSet}
       />
     </CalenderBox>
   );
